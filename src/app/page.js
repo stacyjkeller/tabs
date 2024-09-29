e731778f24995098bc60ba24f809faa3520b83d7
Stacy Keller <stacykeller@Stacys-MBP.home>
2024-09-29 17:11:35 -0400

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,99 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [htmlVisible, setHtmlVisible] = useState(true);
+  const [cssVisible, setCssVisible] = useState(false);
+  const [jsVisible, setJsVisible] = useState(false);
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleHtmlClick = () => {
+    setActiveTab(0);
+    setHtmlVisible(true);
+    setCssVisible(false);
+    setJsVisible(false);
+  };
+
+  const handleCssClick = () => {
+    setActiveTab(1);
+    setHtmlVisible(false);
+    setCssVisible(true);
+    setJsVisible(false);
+  };
+
+  const handleJsClick = () => {
+    setActiveTab(2);
+    setHtmlVisible(false);
+    setCssVisible(false);
+    setJsVisible(true);
+  };
+
   return (
     <div className={styles.page}>
+      <nav role="tablist">
+        <button
+          onClick={handleHtmlClick}
+          className={activeTab === 0 ? "activeTab" : ""}
+          role="tab"
+          id="tab-0"
+          aria-controls="tabpanel=0"
+          aria-selected={activeTab === 0 ? true : false}
+        >
+          HTML
+        </button>
+        <button
+          onClick={handleCssClick}
+          className={activeTab === 1 ? "activeTab" : ""}
+          role="tab"
+          id="tab-1"
+          aria-controls="tabpanel=1"
+          aria-selected={activeTab === 0 ? true : false}
+        >
+          CSS
+        </button>
+        <button
+          onClick={handleJsClick}
+          className={activeTab === 2 ? "activeTab" : ""}
+          role="tab"
+          id="tab-2"
+          aria-controls="tabpanel=2"
+          aria-selected={activeTab === 0 ? true : false}
+        >
+          JavaScript
+        </button>
+      </nav>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div>
+          {htmlVisible && (
+            <section role="tabpanel" id="tabpanel-0" aria-labelledby="tab-0">
+              <p>
+                The HyperText Markup Language or HTML is the standard markup
+                language for documents designed to be displayed in a web
+                browser.
+              </p>
+            </section>
+          )}
+          {cssVisible && (
+            <section role="tabpanel" id="tabpanel-1" aria-labelledby="tab-1">
+              <p>
+                Cascading Style Sheets is a style sheet language used for
+                describing the presentation of a document written in a markup
+                language such as HTML or XML.
+              </p>
+            </section>
+          )}
+          {jsVisible && (
+            <section role="tabpanel" id="tabpanel-2" aria-labelledby="tab-2">
+              <p>
+                JavaScript, often abbreviated as JS, is a programming language
+                that is one of the core technologies of the World Wide Web,
+                alongside HTML and CSS.
+              </p>
+            </section>
+          )}
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
